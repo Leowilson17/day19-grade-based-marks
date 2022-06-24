a694848244ee28f95fa2a4e6fccdf9420d025eb9
leowilson <leobca2018@gmail.com>
2022-06-24 19:38:31 +0530

    add html and css files.

---
+++ script.js
@@ -0,0 +1,14 @@
+let mathsMark = parseInt(prompt());
+console.log("Your Mark is: " , mathsMark);
+if (mathsMark >= 90) {
+    console.log("Grade A")
+}
+else if (mathsMark >= 80) {
+    console.log("Grade B")
+}
+else if (mathsMark >= 50) {
+    console.log("Grade C")
+}
+else {
+    console.log("Fail")
+}
